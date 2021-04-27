5237d31dc4a679bf859c6cf3ff19ac22367a9dae
CharlesDexterW <agarces2381@gmail.com>
2021-04-27 13:27:21 -0500

    Week 3 Assignment

---
+++ Lexical_Scoping.R
@@ -0,0 +1,24 @@
+makecachematrix <- function(x = matrix()){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverese) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get =get, setInverse = setInverse, getInverse =getInverse)
+}
+
+
+cachesolve <- function(x, ...){
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
+}
